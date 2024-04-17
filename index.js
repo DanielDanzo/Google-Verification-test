7a9a9e0be35992e61ba02bdcdefd7d260d252b79
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-17 16:36:04 +0200

    added verification

---
+++ index.js
@@ -0,0 +1,14 @@
+const clients = require('./clients.json');
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    const aTag = document.getElementById('aTag');
+    var client = { 
+        "name": profile.getName(),
+        "Email": profile.getEmail(),
+        "Image":  profile.getImageUrl()
+    }
+
+    clients.push(client);
+    aTag.click();
+}
