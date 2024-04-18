6961f64f0f12499d70b4fe05ae1958e368472017
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-18 14:31:10 +0200

    navigation to home page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     clickButton();
-    
+    window.location.assign("home.html");
 }
 
 
