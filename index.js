ffc9dcabf30e8f91f82af5bddf79ed16c128bc0e
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-18 14:12:57 +0200

    tried using a finction

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    clickButton();
+    
+}
 
+
+
+function clickButton(){
     const aTag = document.getElementById('aTag');
     aTag.click();
 }
