f6d2399716c756ec1e2d1126bf92678e26923e64
DanielDanzo <sempapadaniel123@gmail.com>
2024-04-26 00:29:01 +0200

    working app

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js"
-import { getFirestore, collection, addDoc, getDocs, doc, query, where, orderBy  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, doc, query, where, orderBy, updateDoc, or  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -18,17 +18,74 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 //intialise app variables
+//Get references to buttons
 const addBtn = document.getElementById('addSum');
 const getBtn = document.getElementById('getSum');
-const allInfo = document.getElementById('Client-info');
+const addUserAppBtn = document.getElementById('addUserApp');
+const updateSignInBtn = document.getElementById('updateSignIn');
+const getUserAppBtn = document.getElementById('getUserApp');
+const getOrderedUserApp = document.getElementById('getOrderedUserApp');
+const allInfo = document.getElementById('Client-info'); //This is the part where we display operation status
+const appInfo = document.getElementById('App-info'); //This is where we display info we want to display
 const email = "2508872@students.wits.ac.za";
 var userID = [];
-var userApplications = [];
-var users = [];
+var userApplications = [];  //Array that contains all the user applications in our database
+var users = [];  //Array tha contains all the users we have in our database
+var fundingOpportunities = [];   //Array that contains all the Funding Opportunities we have in the database
 
 
-async function updateSignIn(){
-  console.log('Updated Sign-In');
+//==================================================Users==============================================================
+
+//Function that displays the Applictions made byt specific user
+function displayApplications(){
+  userApplications.forEach((data)=>{
+    const ID = data.userID;
+    const STATUS = data.status;
+    const SUBMITDATE = data.submitDate;
+    const CLOSINGDATE = data.closingDate;
+
+    const IDTag = document.createElement('p');
+    const STATUSTag = document.createElement('p');
+    const SUBMITDATETag = document.createElement('p');
+    const CLOSINGDATETag = document.createElement('p');
+
+    IDTag.textContent = ID;
+    STATUSTag.textContent = STATUS;
+    SUBMITDATETag.textContent = SUBMITDATE;
+    CLOSINGDATETag.textContent = CLOSINGDATE;
+
+    const group = document.createElement('div');
+    group.appendChild(IDTag);
+    group.appendChild(STATUSTag)
+    group.appendChild(SUBMITDATETag)
+    group.appendChild(CLOSINGDATETag)
+
+    group.style.display ="flex";
+    group.style.alignItems = "center"
+    group.style.justifyContent = "space-between";
+    appInfo.appendChild(group);
+  });
+}
+
+
+//TODO: fix it so you can update user information
+async function updateSignIn(userID){
+  
+  try {
+    const q = doc(db, "users", userID);
+    await updateDoc(q, {
+      isSignIn: false, 
+    })
+    .then(()=>{
+      allInfo.textContent = "Updated Sucessfully";
+    })
+    .catch((error)=>{
+      console.error("Error updating document: ", error)
+    });
+    
+  } catch (e) {
+    console.error("Error updating document: ", e);
+}
 }
 
 /*  FUNCTION: Adds user to the database
@@ -47,6 +104,7 @@ async function addUser(email, role, isSignIn, userToken){
           Token: userToken
         });
         userID.push(docRef.id);
+        allInfo.textContent = "Sucessfully Added";
       } catch (e) {
         console.error("Error adding document: ", e);
     }
@@ -61,14 +119,21 @@ async function addUser(email, role, isSignIn, userToken){
 */
 async function addUserApplication(userID, closingDate){
   try {
+
+      // Reference to the user document
+      const userRef = doc(db, 'users', userID);
+
+      // Reference to the subcollection
+      const applicationsRef = collection(userRef, 'Applications');
       const currentDate = new Date().toLocaleDateString();
 
-      const docRef = await addDoc(collection(db, "users",userID,"Applications"), {
+      const docRef = await addDoc(applicationsRef, {
         userID: userID,
         status: "Pending",
         submitDate: currentDate,
         closingDate: closingDate
       });
+      allInfo.textContent = "Added Application Sucessfully";
     } catch (e) {
       console.error("Error adding document: ", e);
   }
@@ -97,6 +162,7 @@ async function getUserApplications(userID){
   querySnapshot.forEach((doc) => {
     userApplications.push(doc.data());
   });
+  displayApplications();
 }
 
 
@@ -119,12 +185,13 @@ async function getSpecificUser(userID){
 */
 async function getAllApplications(userID){
   userApplications = [];
-  //const q = query(collection(db, 'users'), where('email', '==', email), orderBy("submitDate", "desc"));
-  const q = query(collection(db, 'users', userID, 'Applications'), orderBy("submitDate", "desc"));
-  const querySnapshot = await collection(q);
+  const q = query(collection(db, 'users', userID, 'Applications'), where("status", "==", "approved"), orderBy("submitDate", "asc"));
+  const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     userApplications.push(doc.data());
   });
+  
+  displayApplications();
 }
 
 /*   FUNCTION: Used to help us find the userID  of a specific user which will be used through out our query searches
@@ -133,24 +200,66 @@ async function getAllApplications(userID){
 *   This funtion returns the userID of a user
 */  
 async function getUserID(email){
-  const q = query(collection(db, 'users'), where('email', '==', email));
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    userID = doc.id;
-  });
+  try {
+    const q = query(collection(db, 'users'), where('Email', '==', email));
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot);
+    querySnapshot.forEach((doc) => {
+      userID = doc.id;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  
 }
 
 //When the page loads we want to fetch the userID immediately
 window.onload = getUserID(email);
 
-//=========================================OUR Button Event Listeners =======================================
+//======================================Funding Opportunities===========================================================
+async function createFundingOportunity(FOName,type,budget,description){
+  try {
+    const docRef = await addDoc(collection(db, "Funding Opportunity"), {
+      Name: FOName,
+      Type: type,
+      Budget: budget,
+      Description: description
+    });
+    fundingOpportunities.push(docRef.id);
+    allInfo.textContent = "Sucessfully Added";
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+
+//=========================================OUR Button Event Listeners ==================================================
 addBtn.addEventListener('click', ()=>{
-    addUser();
+    addUser(email,"Admin",true,"Token");
 });
 
 getBtn.addEventListener('click', ()=>{
+  //getUserID(email);
   getUsers();
 });
+
+addUserAppBtn.addEventListener('click', ()=>{
+  addUserApplication(userID, new Date().toLocaleDateString());
+});
+
+getUserAppBtn.addEventListener('click',()=>{
+  getUserApplications(userID);
+});
+
+getOrderedUserApp.addEventListener('click',()=>{
+  getAllApplications(userID);
+});
+
+updateSignInBtn.addEventListener('click',()=>{
+  updateSignIn(userID);
+});
+
+
 
 
 
